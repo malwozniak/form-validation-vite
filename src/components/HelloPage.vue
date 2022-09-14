5cf0ebf50a9258d17d64c7b1e7548c517c4961f7
malwozniak <mgrztwnk5@gmail.com>
2022-09-14 11:16:38 +0200

    4 files modified, 1 added

--- src/components/HelloPage.vue
+++ src/components/HelloPage.vue
@@ -1,40 +1,46 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+  <div>
+    <h1>Hi {{ account.user.firstName }}!</h1>
+    <p>You're logged in with Vue + Vuex & JWT!!</p>
+    <h3>Users from secure api end point:</h3>
+    <em v-if="users.loading">Loading users...</em>
+    <span v-if="users.error" class="text-danger">ERROR: {{ users.error }}</span>
+    <ul v-if="users.items">
+      <li v-for="user in users.items" :key="user.id">
+        {{ user.firstName + ' ' + user.lastName }}
+        <span v-if="user.deleting"><em> - Deleting...</em></span>
+        <span v-else-if="user.deleteError" class="text-danger">
+          - ERROR: {{ user.deleteError }}</span
+        >
+        <span v-else>
+          - <a @click="deleteUser(user.id)" class="text-danger">Delete</a></span
+        >
+      </li>
+    </ul>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <router-link to="/login">Logout</router-link>
     </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState({
+      account: (state) => state.account,
+      users: (state) => state.users.all,
+    }),
+  },
+  created() {
+    this.getAllUsers();
+  },
+  methods: {
+    ...mapActions('users', {
+      getAllUsers: 'getAll',
+      deleteUser: 'delete',
+    }),
+  },
+};
+</script>
