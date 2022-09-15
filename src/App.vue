4cfacc740b3f263734c765a89ceb259d470522cb
malwozniak <mgrztwnk5@gmail.com>
2022-09-15 11:57:25 +0200

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,22 @@
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
 
-    <div class="jumbotron">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 offset-sm-3">
-                    <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
-                    <router-view></router-view>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
+import { Nav, Alert } from '@/components';
+import { useAuthStore } from '@/stores';
 
-<script>
-import { mapState, mapActions } from 'vuex'
-
-export default {
-    name: 'app',
-    computed: {
-        ...mapState({
-            alert: state => state.alert
-        })
-    },
-    methods: {
-        ...mapActions({
-            clearAlert: 'alert/clear' 
-        })
-    },
-    watch: {
-        $route (to, from){
-            // clear alert on location change
-            this.clearAlert();
-        }
-    } 
-};
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloPage.vue';
+const authStore = useAuthStore();
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<template>
+  <div class="app-container" :class="authStore.user && 'bg-light'">
+    <Nav />
+    <Alert />
+    <div class="container pt-4 pb-4">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style>
+@import '@/assets/base.css';
 </style>
