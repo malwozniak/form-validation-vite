7cb5615e3ea42b5c06ccf74a01b67f99ab16b2ee
malwozniak <mgrztwnk5@gmail.com>
2022-09-15 11:53:48 +0200

    1 file modified

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
-import { router } from 'router';
+import { router } from 'vue-router';
 import { fakeBackend } from './helpers';
 fakeBackend();
 const app = createApp(App);
